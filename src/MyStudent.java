a614303c66e9eac0d4f6da3545b8580658dcb60e
divyanshi verma <divyanshi.2428csea@kiet.edu>
2025-08-22 12:25:52 +0530

    h

---
+++ src/MyStudent.java
@@ -0,0 +1,7 @@
+public class MyStudent {
+    public static void main(String [] args){
+        Student2 obj=new Student2("A",12,"Rahul");
+        System.out.println(obj.name);
+        System.out.println(obj.getGrade());
+    }
+}
